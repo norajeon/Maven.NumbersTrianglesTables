383d15a7a0fddbe0a1bccc0b23066d3309d84b7a
Nora <sxj042@gmail.com>
2023-03-04 00:25:14 -0500

    finished

--- src/main/java/io/zipcoder/microlabs/mastering_loops/TriangleUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/TriangleUtilities.java
@@ -3,18 +3,51 @@ package io.zipcoder.microlabs.mastering_loops;
 public class TriangleUtilities {
 
     public static String getTriangle(int numberOfRows) {
-        return null;
+        StringBuilder stars = new StringBuilder();
+        for (int i = 1; i < numberOfRows ; i++) {
+            for (int j = 1; j <= i; j++) {
+                stars.append("*");
+            }
+            stars.append("\n");
+        }
+        return stars.toString();
     }
 
     public static String getRow(int numberOfStars) {
-        return null;
+        // we want numberOfStars amount of ***
+        StringBuilder stars = new StringBuilder();
+        for (int i = 1; i <= numberOfStars; i++) {
+            stars.append("*");
+        }
+
+        return stars.toString();
     }
 
     public static String getSmallTriangle() {
-        return null;
+    StringBuilder stars = new StringBuilder();
+    for (int i = 1; i <= 4 ; i++) {
+        for (int j = 1; j <= i; j++) {
+
+
+            stars.append("*");
+
+        }
+        stars.append("\n");
+    }
+        return stars.toString();
     }
 
     public static String getLargeTriangle() {
-        return null;
+        StringBuilder stars = new StringBuilder();
+        for (int i = 1; i <= 9 ; i++) {
+            for (int j = 1; j <= i; j++) {
+
+
+                stars.append("*");
+
+            }
+            stars.append("\n");
+        }
+        return stars.toString();
     }
 }
